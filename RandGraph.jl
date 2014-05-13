efd2ee6f5de74ca79c2dc96604afbb4ee23deefb
Trezitorul <trezitorul@gmail.com>
2014-05-12 23:04:49 -0400

    Updated the random graph utility function and added comments, additionally added Ms to make code a bit more logical

--- RandGraph.jl
+++ RandGraph.jl
@@ -1,77 +1,41 @@
 #Random Graph Generator G(n,m)
 #The purpose of this file is to demonstrate to Joaquin how much more baller Julia is than Matlab.
 
-
+export Gr, Ms
+#This function generates an undirected graph with N nodes and M edges. The M edges are uniformly distributed between the N nodes.
+#Inputs:
+#N: Number of nodes in the graph
+#M: Number of edges in the graph
+#Output:
+#Integer Adjacency matrix
 function Gr(N,M)
-	G=zeros(Integer,N,N)
-	VL=[0,0]
-	test=true
-	x=0
-	y=0
-	for i=1:M
-		#print("Generating Edge #")
-		#println(i)
-		while test
-			VL=ifloor((N-1)*rand(2))
-			if G[VL[1]+1,VL[2]+1]==0 && (VL[1]+1)!=(VL[2]+1)
-				test=false
+	G=zeros(Integer,N,N)#Generate empty graph of zeros
+	VL=[0,0]#The node pair between which an edge is generated
+	test=true#Gate Variable that checks for repetition
+	for i=1:M#Loops to generate M edges
+		while test#Test assumes there is a duplicate edge or a self edge
+			VL=ifloor((N-1)*rand(2))#Generates random pairing of vertices
+			if G[VL[1]+1,VL[2]+1]==0 && (VL[1]+1)!=(VL[2]+1)#If there is no edge present or self edge
+				test=false#Set gate variable that edge is valid
 			end
 		end
-		test=true
-		#print("Between the Following Vertices:")
-		#print(VL[1])
-		#print(",")
-		#println(VL[2])
-		G[VL[1]+1,VL[2]+1]=1
-		G[VL[2]+1,VL[1]+1]=1
-
+		test=true#Reset Gate variable
+		G[VL[1]+1,VL[2]+1]=1#Sets randomly chosen edge to 1 
+		G[VL[2]+1,VL[1]+1]=1#Sets transpose value since our graph is simple graph
 	end
-	return G
+	return G#Returns the graph generated
 end
 
-function experiment1()
-	Ns=[50,100,200,300,500,700,800,900]
-	Nu=1000
-	Mu=100000
-	out=zeros(Integer,Mu,length(Ns))
-	println(size(out))
-	runtimes=10000
-	s=0
-	for i=1:runtimes
-		println(100*i/runtimes)
-		A=Gr(Nu,Mu)
-		for n=1:length(Ns)
-			#println([1:Ns[n]])
-			s=0
-			#println(size(A))
-			#println(size(getindex(A,[1:Ns[n]],[1:Ns[n]])))
-			s=sum(getindex(A,[1:Ns[n]],[1:Ns[n]]))
-			#println(s+1)
-			s=s/2+1
-			out[s,n]=out[s,n]+1
-			#println("hello2")
-		end
-	end
-	return out
+#Generates the number of edges that are present in a sub system Ns given a graph with Nu nodes and Mu edges
+#Inputs:
+#Nu, number of nodes present in the graph
+#Mu, number of edges present in the graph
+#Ns, number of nodes in our system
+#Output:
+#Total Number of edges present in our subgraph.
+function Ms(Nu,Mu,Ns)
+	U=G(Nu,Mu)#Generate Matrix
+	return (.5)*sum(getindex(U,[1:Ns],[1:Ns]))#Get the number of edges present in the subsystem
 end
 
-function experiment2()
-	Nu=1000
-	Ns=10
-	Mu=[10,50,100,200,240,250,300,400]
-	Mu=1000*Mu
-	out=zeros(Integer,400000,length(Mu))
-	s=0
-	runtimes=10000
-	for i=1:length(Mu)
-		for k=1:runtimes
-			println(100*k/runtimes)
-			s=0
-			A=Gr(Nu,Mu[i])
-			s=sum(getindex(A,[1:Ns],[1:Ns]))
-			s=s/2+1
-			out[s,i]
-		end
-	end
-	return out
-end
+
